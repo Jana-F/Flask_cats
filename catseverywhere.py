af03fc4f4054a3d4854ceea34f8d090f3ccb7b5e
Jana-F <jana@fedra.cz>
2017-07-08 23:45:39 +0200

    Update catseverywhere.py

--- catseverywhere.py
+++ catseverywhere.py
@@ -5,21 +5,33 @@ app = Flask(__name__)
 
 email_addresses = []
 
+
 @app.route('/')
 def hello_world():
     datum = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template('index.html', datum=datum)
 
-@app.route('/signup', methods = ['POST'])
+
+@app.route('/signup', methods=['POST'])
 def signup():
     email = request.form['email']
     email_addresses.append(email)
     print(email_addresses)
     return redirect('/')
 
+
 @app.route('/emails.html')
 def emails():
     return render_template('emails.html', email_addresses=email_addresses)
+
+
+@app.route('/static/cat.jpg')
+def link():
+    if request.method == 'POST':
+        return redirect(url_for('/static/cat.jpg'))
+    else:
+        return render_template('/')
+
 
 if __name__ == '__main__':
     app.run()
